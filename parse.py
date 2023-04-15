20406a7ae1c460474115b371fc609f675c96a062
sanju2728 <sanjanabhat2002@gmail.com>
2023-04-15 21:04:31 +0530

    fix: change quote source

--- parse.py
+++
@@ -1,6 +0,0 @@
-f = open("./f.txt", encoding = "utf-8").read().split("\n")
-for i in range(len(f)):
-    f[i] = f[i].replace("“", "")
-    f[i] = f[i].replace("”", "")
-    f[i] = f[i].replace("’", "'")
-print(f)
